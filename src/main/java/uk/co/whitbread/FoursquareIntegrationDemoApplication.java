d2d752d3566b2b22ff57c8692eb50db571724ac6
Oleksandr Murha <oleksandr.murha@gmail.com>
2016-10-13 14:49:21 +0100

    Initial commit

---
+++ src/main/java/uk/co/whitbread/FoursquareIntegrationDemoApplication.java
@@ -0,0 +1,12 @@
+package uk.co.whitbread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoursquareIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoursquareIntegrationDemoApplication.class, args);
+	}
+}
