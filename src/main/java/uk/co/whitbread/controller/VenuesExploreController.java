59b2c7d2a0b1842b700d30b009d7d46c5aa4c690
Oleksandr Murha <oleksandr.murha@gmail.com>
2016-10-13 18:05:03 +0100

    Add support for empty location

--- src/main/java/uk/co/whitbread/controller/VenuesExploreController.java
+++ src/main/java/uk/co/whitbread/controller/VenuesExploreController.java
@@ -3,6 +3,7 @@ package uk.co.whitbread.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
@@ -14,23 +15,29 @@ import uk.co.whitbread.model.foursquare.VenuesExploreResponse;
 import uk.co.whitbread.utils.UrlBuilder;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 
 @Controller
 public class VenuesExploreController {
 
+    private static final String TYPE_LOCATION = "Please type location into search box";
+    private static final String RECOMENDED_PLACES = "Recommended locations around %s!";
     @Autowired
     private UrlBuilder urlBuilder;
 
     private RestTemplate restTemplate = new RestTemplate();
 
     @RequestMapping("/explore.html")
-    public String exploreVenue(@RequestParam String location, Model model) throws Exception {
-        VenuesExploreResponse venuesExploreResponse = restTemplate.getForObject(urlBuilder.buildUrl(location), VenuesExploreResponse.class);
-        List<RecommendedLocation> recommendedLocations = venuesExploreResponse.getResponse().getGroups().stream().flatMap(group -> group.getItems().stream()).map(Item::getVenue).map(this::convert).collect(toList());
-        model.addAttribute("name", venuesExploreResponse.getResponse().getHeaderFullLocation());
-        model.addAttribute("recommendedLocations", recommendedLocations);
+    public String exploreVenue(@RequestParam(required = false) String location, Model model) throws Exception {
+        model.addAttribute("caption", TYPE_LOCATION);
+        if (StringUtils.hasLength(location)) {
+            VenuesExploreResponse venuesExploreResponse = restTemplate.getForObject(urlBuilder.buildUrl(location), VenuesExploreResponse.class);
+            List<RecommendedLocation> recommendedLocations = venuesExploreResponse.getResponse().getGroups().stream().flatMap(group -> group.getItems().stream()).map(Item::getVenue).map(this::convert).collect(toList());
+            model.addAttribute("recommendedLocations", recommendedLocations);
+            model.addAttribute("caption", String.format(RECOMENDED_PLACES, venuesExploreResponse.getResponse().getHeaderFullLocation()));
+        }
         return "explore";
     }
 
