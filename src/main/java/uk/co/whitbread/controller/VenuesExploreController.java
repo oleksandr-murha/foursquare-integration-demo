51d7101eb0a7f1208b3cae33622b6ddb6145b1bf
Oleksandr Murha <oleksandr.murha@gmail.com>
2016-10-13 15:54:12 +0100

    Add request param to VenuesExploreController

--- src/main/java/uk/co/whitbread/controller/VenuesExploreController.java
+++ src/main/java/uk/co/whitbread/controller/VenuesExploreController.java
@@ -2,20 +2,22 @@ package uk.co.whitbread.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import uk.co.whitbread.model.VenuesExploreResponse;
-import uk.co.whitbread.properties.FoursquareProperties;
+import uk.co.whitbread.utils.UrlBuilder;
 
 @RestController
 public class VenuesExploreController {
 
-    String url = "https://api.foursquare.com/v2/venues/explore?client_id=01SQBD4NF1YIWMEBJHB2VQG1HFKICH5XKT5WXQIMUQ012X3P&client_secret=PELK3BQ3DVRC4A24SPZSP5DUHPGIC5SEUORUAABNDFOI54GV&near=London&v=20140806&m=foursquare";
+    @Autowired
+    private UrlBuilder urlBuilder;
 
     private RestTemplate restTemplate = new RestTemplate();
 
     @RequestMapping("/explore")
-    public VenuesExploreResponse exploreVenue(String location) throws Exception {
-        return restTemplate.getForObject(url, VenuesExploreResponse.class);
+    public VenuesExploreResponse exploreVenue(@RequestParam String location) throws Exception {
+        return restTemplate.getForObject(urlBuilder.buildUrl(location), VenuesExploreResponse.class);
     }
 }
